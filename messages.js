0f92dab6e5aa83ff45cf256c5ad68971604450ca
caisse43700-lgtm <kassem43700@gmail.com>
2026-05-20 01:48:43 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -653,15 +653,8 @@
 
       try { if (typeof App.closeOverlay === 'function') App.closeOverlay('reportPanel'); } catch (e) {}
 
-      try {
-  App.openSheet && App.openSheet();
-} catch(e) {}
-
-document.querySelectorAll('.panel').forEach(p => p.classList.remove('on'));
-document.getElementById('panelMessages')?.classList.add('on');
-
-document.querySelectorAll('.sheet-nav button').forEach(b => b.classList.remove('on'));
-document.getElementById('tabMessages')?.classList.add('on');
+      App.panel('messages');
+setMode('compose');
       setMode('compose');
 
       setTimeout(() => {
